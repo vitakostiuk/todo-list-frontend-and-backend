08eba79fa1136c7519a862f7c17f02cfc654cca6
vitakostiuk <vitagrebennik@gmail.com>
2022-10-19 22:41:37 +0300

    bug: Add type to request parameter

--- packages/frontend/src/modules/common/todoForm/todoForm.component.tsx
+++ packages/frontend/src/modules/common/todoForm/todoForm.component.tsx
@@ -1,8 +1,8 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React from 'react';
 import { Formik, Form, Field } from 'formik';
-import { useQuery } from 'react-query';
-import TodoService from '../../services/todo.service';
+// import { useQuery } from 'react-query';
+// import TodoService from '../../services/todo.service';
 
 interface MyFormValues {
   title: string;
@@ -12,8 +12,8 @@ interface MyFormValues {
 
 export const TodoForm: React.FC<{}> = () => {
   const initialValues: MyFormValues = { title: '', description: '', private: false };
-  const { data } = useQuery('all todos', () => TodoService.getAllTodos());
-  console.log(data);
+  // const { data } = useQuery('all todos', () => TodoService.getAllTodos());
+  // console.log(data);
 
   // const { data:response, isLoading,error, status } = useQuery('add todo', () => TodoService.addTodo());
   // console.log(data);
@@ -32,7 +32,7 @@ export const TodoForm: React.FC<{}> = () => {
       <Formik
         initialValues={initialValues}
         onSubmit={(values, actions) => {
-          console.log({ values, actions });
+          // console.log({ values, actions });
           actions.setSubmitting(false);
         }}
       >
