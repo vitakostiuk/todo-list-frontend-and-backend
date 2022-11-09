fd4b3f3732b0a759e789f56268757e87ffe08328
vitakostiuk <vitagrebennik@gmail.com>
2022-11-09 12:46:25 +0200

    feat: Add browserRouter

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import AppContainer from './modules/app';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppContainer />
+    <BrowserRouter
+      basename="/
+todo-list-frontend-and-backend
+/"
+    >
+      <AppContainer />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
